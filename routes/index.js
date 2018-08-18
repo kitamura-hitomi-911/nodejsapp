a0a5e0963a3ae2f4957cd41b1202b2864d088c50
hitomi <hinitial@gmail.com>
2018-08-18 16:10:05 +0900

    sqlite3追加

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var sqlite3 = require('sqlite3');
+var db = new sqlite3.Database('mydb.sqlite3');
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res, next){
+	db.serialize(() => {
+		db.all("select * from mydata", (err, rows) => {
+			if(!err){
+				console.log(rows);
+				res.render('index', {title: 'Express'});
+			}
+		})
+	});
 });
-
 module.exports = router;
